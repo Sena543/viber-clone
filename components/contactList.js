c8998cbf1b50412c00e7c578baa6989d06aae6bf
Senanu543 <senanuazanu@gmail.com>
2020-02-15 22:51:39 +0000

    Created the chat list and added some contacts

--- components/contactList.js
+++ components/contactList.js
@@ -1,19 +1,50 @@
 import React from 'react'
-import {FlatList, Text, StyleSheet, View} from 'react-native';
+import {FlatList, Text, StyleSheet, View, Image} from 'react-native';
+import { Ionicons, FontAwesome, Octicons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function ContactList(){
     const contacts =[
         {
-         id:1,
-        name:'Maya'}, 
+            id:1,
+            name:'Martin Kuerbs',
+            message: 'Send me the docs please',
+            time: '09:28',
+            messagenumber: 8,
+            avatarPath: require('../assets/images/lantern.jpg'),
+        }, 
         {
-          id:2,
-          name:'Julia'
-        },
-      {
-        id:3,
-        name:'Mike'}]
+            id:2,
+            name:'FC Barcelona',
+            message: 'Valerdy: ',
+            time: '09:28',
+            messagenumber: 2,
+            avatarPath: require('../assets/images/match.jpg'),
+           },
+        {
+            id:3,
+            name:'Shakira',
+            message: "A tour is on it's, are you ready? Bacelona tickets",
+            time: '12:30',
+            messagenumber: 3,
+            avatarPath: require('../assets/images/fire.jpg'),
+            },
+        {
+            id:4,
+            name:'Giovanni Moretti',
+            message: "Not Sure I can come today.Very busy this week",
+            time: '22:20',
+            messagenumber: 1,
+            avatarPath: require('../assets/images/bulb.jpg'),
+            },
+        {
+            id:5,
+            name:'Alex Kulnig',
+            message: "Sticker",
+            time: '1:30',
+            messagenumber: 5,
+            avatarPath: require('../assets/images/batman.jpg'),
+            }]
     
     
         return(
@@ -23,24 +54,55 @@ export default function ContactList(){
                     keyExtractor ={(item)=> item.id.toString()}
                     renderItem={({item})=>(
                         <TouchableOpacity>
-                            <Text>{item.name}</Text>
+                            <View style= {styles.container}>
+                                <View style={styles.main} >
+                                    <View style={{flex:2,margin:10}}>
+                                        <Image source={item.avatarPath} size={10} style={styles.imageStyle} />
+                                    </View>
+                                    <View style={{flex:6, margin:10}}>
+                                        <Text style={{fontSize:30, color:'#ffffff'}}>{item.name}</Text>
+                                        <Text style={{fontSize:15, color:'#80818a'}}>{item.message}</Text>
+                                    </View>
+                                    <View style={{flex:1.5, flexDirection:'row', justifyContent:'flex-end', }}>
+                                        <Ionicons size={20} color='#80818a' name='md-checkmark'/>
+                                    <View style={{margin:5}}>
+                                        <Text style={{fontSize:15, color:'#4a03a6'}}>{item.time}</Text>
+                                        <View style={{flex:1,alignItems:'center',justifyContent:'center', margin:15 }}>
+                                            < Text style={styles.messages}>{item.messagenumber}</Text>
+                                        </View>
+                                    </View>
+                                    </View>
+                                </View>
+                            </View>     
                         </TouchableOpacity>
                     )}
                 />
             </View>
-        )
-
-}
+        )}
 
 const styles = StyleSheet.create({
-    container:{
-        flex:2,
-        backgroundColor: '#fff', 
-        justifyContent: 'center'
+    container: {
+        flex: 1,
+        backgroundColor:'#152238'
+      },
+    main:{
+        flex:1,
+        backgroundColor:'#152238', 
+        height:80,
+        borderRadius:20,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexDirection:'row'
     },
-    testStyle:{
-        fontSize:20,
-        
+    imageStyle:{
+        height:90, 
+        width:90, 
+        borderRadius:100
+    },
+    messages:{
+        backgroundColor:'#4a03a6',
+        borderRadius:100,
+        fontSize:15,
+        color:'#ffffff'
     }
-
 }) 
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -2,7 +2,11 @@ import React from 'react';
 import {
   StyleSheet,
   View,
+  Image,
+  Text
 } from 'react-native';
+import { Ionicons, FontAwesome, Octicons } from '@expo/vector-icons';
+//import { Divider } from 'react-native-elements'
 
 import { MonoText } from '../components/StyledText';
 import ContactList from '../components/contactList';
@@ -11,7 +15,9 @@ export default function ChatScreen() {
   
   return (
    <View style= {styles.container}>
+   
       <ContactList/>
+        
    </View>
   );
 }
@@ -19,14 +25,15 @@ export default function ChatScreen() {
 ChatScreen.navigationOptions = {
   title: 'Chats',
   headerStyle:{
-    backgroundColor:'#152238'
+    backgroundColor:'#152238',
+    color: 'white'
   }
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'#152238',
   },
   textStyle:{
     fontSize: 30,
